69ed7632d830ea1780de4374d2df562576c07265
Y0SH4 <yosa.agung13@gmail.com>
2023-09-22 19:07:59 +0700

    update

--- src/pages/nft-farming.tsx
+++ src/pages/nft-farming.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  LayoutMainV2,
-  ModalAnnouncement,
-  TableRankNFTBonusV2,
-} from "components";
+import { LayoutMainV2 } from "components";
 import { withConnection } from "hoc";
 import {
   SectionGnetProject,
@@ -16,14 +12,10 @@ import { Box } from "@chakra-ui/react";
 const NftFarmingV2 = () => {
   return (
     <LayoutMainV2>
-      {/* <ModalAnnouncement/> */}
       <Box bgGradient="linear-gradient(180deg, #2C1FA7 0%, #6D02C9 100%)">
         <SectionNFTList />
         <SectionGnetProject />
         <SectionMyNFTV2 />
-      </Box>
-      <Box bgGradient="linear(to-b, #6D02C9, #2C1FA7, #6D02C9)">
-        <TableRankNFTBonusV2 />
       </Box>
       <SectionMatchingRequirment />
     </LayoutMainV2>
