0d529c5fb10ace9877584fdd7ec198fc5a8a74b9
Y0SH4 <yosa.agung13@gmail.com>
2023-09-29 01:51:27 +0700

    update

--- src/hooks/useCardList.ts
+++ src/hooks/useCardList.ts
@@ -1,14 +1,9 @@
 import { useEffect, useState } from "react";
 import { NFT } from "valhalla-erc20/typechain-types";
-import { useNFTContract } from "./useNFTContract";
 import { useNFTFolkContract } from "./useNFTFolkContract";
 import { BigNumber, BigNumberish } from "ethers";
-import { useGNETContract } from "./useGNETContract";
 import { useAddress, useContractWrite } from "@thirdweb-dev/react";
 import { useAccountMap, useIsRankRewardClaimable } from "./valhalla";
-import { useMaxBuy } from "./nft/useMaxBuy";
-import { MAX_BUY } from "constant/maxbuy";
-import { toBn } from "evm-bn";
 import { useFLDContract } from "./useFLDContract";
 import { useUSDTContract } from "./useUSDTContract";
 
@@ -24,10 +19,11 @@ export const useCardList = () => {
   const fld = useFLDContract();
   const usdt = useUSDTContract();
   const address = useAddress();
-  const isRankRewardClaimable = useIsRankRewardClaimable();
   const approveFld = useContractWrite(fld.contract, "approve");
   const approveUsdt = useContractWrite(usdt.contract, "approve");
-  const buyNft = useContractWrite(nft.contract, "buyCardWithUSDT");
+  const buyNftWithFLD = useContractWrite(nft.contract, "buyCardWithFLD");
+  const buyNftWithUSDT = useContractWrite(nft.contract, "buyCardWithUSDT");
+
   const { data: account } = useAccountMap();
   const [data, setData] = useState<CardType[]>([]);
   const [isLoading, setLoading] = useState(true);
@@ -56,78 +52,64 @@ export const useCardList = () => {
 
   const buy = async (tokenId: BigNumberish, currency: number) => {
     if (!fld.contract || !nft.contract || !address || !usdt.contract) return;
+    const card = await nft.contract!.call("cardMap", [tokenId]);
+    const cardPrice = card.price;
+    const fldBalance = await fld.contract.call("balanceOf", [address]);
+    const usdtBalance = await usdt.contract.call("balanceOf", [address]);
+    const allowanceFld = await fld.contract.call("allowance", [
+      address,
+      nft.contract.getAddress(),
+    ]);
+    const allowanceUsdt = await usdt.contract.call("allowance", [
+      address,
+      nft.contract.getAddress(),
+    ]);
 
-    if (currency === 1) {
-      const card = await nft.contract!.call("cardMap", [tokenId]);
-      const cardPrice = card.price;
-      const fldBalance = await fld.contract.call("balanceOf", [address]);
-      const allowance = await fld.contract.call("allowance", [
-        address,
-        nft.contract.getAddress(),
-      ]);
-
-      if (cardPrice.gt(fldBalance)) {
-        throw {
-          code: "NotEnoughFldBalance",
-        };
-      }
-
-      if (cardPrice.gt(allowance)) {
-        await approveFld.mutateAsync({
-          args: [nft.contract.getAddress(), cardPrice.mul(10)],
-        });
-      }
+    if (currency === 0) {
       if (!account?.isRegistered) {
         throw {
           code: "RegistrationRequired",
         };
       }
-      // if (isRankRewardClaimable.data) {
-      //   throw {
-      //     code: "RankStarted",
-      //   };
-      // }
-      const receipt = await buyNft.mutateAsync({ args: [tokenId] });
-      return receipt;
-    } else if (currency === 0) {
-      const card = await nft.contract!.call("cardMap", [tokenId]);
-      const cardPrice = card.price;
-      const usdtBalance = await usdt.contract.call("balanceOf", [address]);
-      const allowance = await usdt.contract.call("allowance", [
-        address,
-        nft.contract.getAddress(),
-      ]);
-
       if (cardPrice.gt(usdtBalance)) {
         throw {
           code: "NotEnoughUsdtBalance",
         };
       }
 
-      if (cardPrice.gt(allowance)) {
+      if (cardPrice.gt(allowanceUsdt)) {
         await approveUsdt.mutateAsync({
           args: [nft.contract.getAddress(), cardPrice.mul(10)],
         });
       }
-      if (!account?.isRegistered) {
-        throw {
-          code: "RegistrationRequired",
-        };
-      }
-      // if (isRankRewardClaimable.data) {
-      //   throw {
-      //     code: "RankStarted",
-      //   };
-      // }
-      const receipt = await buyNft.mutateAsync({ args: [tokenId] });
+      const receipt = await buyNftWithUSDT.mutateAsync({ args: [tokenId] });
       return receipt;
     }
+
+    if (!account?.isRegistered) {
+      throw {
+        code: "RegistrationRequired",
+      };
+    }
+    if (cardPrice.gt(fldBalance)) {
+      throw {
+        code: "NotEnoughFldBalance",
+      };
+    }
+
+    if (cardPrice.gt(allowanceFld)) {
+      await approveFld.mutateAsync({
+        args: [nft.contract.getAddress(), cardPrice.mul(10)],
+      });
+    }
+
+    const receipt = await buyNftWithFLD.mutateAsync({ args: [tokenId] });
+    return receipt;
   };
 
   return {
     isLoading: isLoading || nft.isLoading,
     data,
     buy,
-    // buy
   };
 };
