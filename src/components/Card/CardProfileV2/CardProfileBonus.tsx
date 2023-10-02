d51dca46d06ca6cb3a3f70f7af263a1ed4aed5aa
Y0SH4 <yosa.agung13@gmail.com>
2023-10-02 10:15:30 +0700

    update

--- src/components/Card/CardProfileV2/CardProfileBonus.tsx
+++ src/components/Card/CardProfileV2/CardProfileBonus.tsx
@@ -4,7 +4,13 @@ import {
   WidgetProfileBtn,
 } from "components/Widget/WidgetProfile";
 import { CURRENT_CHAIN_ID, useAsyncCall } from "hooks";
-import { useBalance, useContract, useContractWrite } from "@thirdweb-dev/react";
+import {
+  useAddress,
+  useBalance,
+  useContract,
+  useContractRead,
+  useContractWrite,
+} from "@thirdweb-dev/react";
 import { t } from "i18next";
 import { CardProfileV2 } from "./CardProfileV2";
 import { fromBn } from "evm-bn";
@@ -13,16 +19,15 @@ import {
   useRewardMap,
   useGlobalPool,
   useIsRankRewardClaimable,
+  useAccountMap,
 } from "hooks/valhalla";
 import { useValhallaContract } from "hooks/useValhallaContract";
-import { USDT_CONTRACT } from "constant/address";
+import { FLD_CONTRACT, USDT_CONTRACT } from "constant/address";
 import { BigNumber } from "ethers";
+import { useNFTFolkContract } from "hooks/useNFTFolkContract";
+import { prettyBn } from "utils";
 
 export const CardProfileBonus = () => {
-  // const { claimRankReward, globalPool, isRankRewardClaimable } = useValhalla();
-  const globalPool = useGlobalPool();
-  const usdt = useBalance(USDT_CONTRACT[CURRENT_CHAIN_ID]);
-  const isRankRewardClaimable = useIsRankRewardClaimable();
   const rewardMap = useRewardMap();
   const rankReward = useRankReward();
   const valhalla = useValhallaContract();
@@ -44,41 +49,33 @@ export const CardProfileBonus = () => {
     await rewardMap.refetch();
   };
 
+  // my code
+  const address = useAddress();
+  const nftFolk = useNFTFolkContract();
+  const fld = useBalance(FLD_CONTRACT[CURRENT_CHAIN_ID]);
+
+  const { data: account } = useAccountMap();
+  const { data: personalBuy } = useContractRead(
+    nftFolk.contract,
+    "personalBuy",
+    [address]
+  );
+
   return (
     <Stack>
       <Stack gap={"3"} w={"full"}>
-        {/* <WidgetProfileBalace bg="#0B5454" px="1rem" rounded="xl">
-          <HStack w={"full"} justifyContent={"space-between"}>
-            <Text>{t("common.globalBonus")}</Text>
-            <Text textAlign={"end"}>
-              {!isRankRewardClaimable?.data
-                ? fromBn(
-                    globalPool?.data?.claimable
-                      ? globalPool?.data?.claimable
-                      : BigNumber.from(0),
-                    6
-                  )
-                : globalPool?.data?.valueLeft &&
-                  fromBn(
-                    globalPool?.data?.valueLeft
-                      ? globalPool?.data?.valueLeft
-                      : BigNumber.from(0),
-                    6
-                  )}{" "}
-              {usdt.data?.symbol}
-            </Text>
-          </HStack>
-        </WidgetProfileBalace> */}
         <WidgetProfileBalace bg="#0B5454" px="1rem" rounded="xl">
           <HStack w={"full"} justifyContent={"space-between"}>
-            <Text>{t("common.PersonalBuy")}</Text>
-            <Text textAlign={"end"}>7000</Text>
+            <Text>{t("common.PersonalNft")}</Text>
+            <Text textAlign={"end"}>
+              {`${prettyBn(personalBuy, 18)} ${fld.data?.symbol}`}
+            </Text>
           </HStack>
         </WidgetProfileBalace>
         <WidgetProfileBalace bg="#0B5454" px="1rem" rounded="xl">
           <HStack w={"full"} justifyContent={"space-between"}>
             <Text>{t("common.TotalNetworkMember")}</Text>
-            <Text textAlign={"end"}>0 Member</Text>
+            <Text textAlign={"end"}>{`${account?.totalDownline} Member`}</Text>
           </HStack>
         </WidgetProfileBalace>
         <WidgetProfileBalace bg="#0B5454" px="1rem" rounded="xl">
@@ -86,8 +83,7 @@ export const CardProfileBonus = () => {
             <Stack>
               <Text>{t("common.sponsor")}</Text>
               <Text>
-                {rewardMap.data && fromBn(rewardMap.data, 6)}{" "}
-                {/* {usdt.data?.symbol} */} FLD
+                {rewardMap.data && fromBn(rewardMap.data, 6)} {fld.data?.symbol}
               </Text>
             </Stack>
             <WidgetProfileBtn
--- src/components/pages/Profile/SectionProfileV2.tsx
+++ src/components/pages/Profile/SectionProfileV2.tsx
@@ -1,89 +1,13 @@
 import {
-  CardProfileBonus,
   CardProfileRankV2,
   CardProfileAddress,
   CardProfileBalanceV2,
 } from "components/Card";
-import axios from "axios";
 import { t } from "i18next";
-import { useMemo } from "react";
-import { User } from "@prisma/client";
-import { getWallet } from "lib/contractFactory";
-import { useModal } from "@ebay/nice-modal-react";
-import { ModalBindTelegram } from "components/Modal";
-import { useMe } from "hooks";
-import { getTelegramBindingSignatureMessage } from "utils";
+
 import { Heading, Stack, Flex, Box } from "@chakra-ui/react";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { WidgetProfileMember } from "components/Widget/WidgetProfile";
-import { useAddress } from "@thirdweb-dev/react";
-import { useAccountMap } from "hooks/valhalla";
-import { useSummary } from "hooks/user";
-import { ZERO_ADDRESS } from "constant/address";
 
 export const SectionProfileV2 = () => {
-  const address = useAddress() ?? ZERO_ADDRESS;
-  const {
-    data: summaryData,
-    isLoading: summaryLoading,
-    error,
-  } = useSummary(address);
-  const accountMap = useAccountMap();
-  const { data, isLoading } = useMe();
-  const user = useMemo<User>(() => {
-    const convert = data
-      ?.filter(e => e.address === address.toLowerCase())
-      .reduce((acc, cv) => ({ ...cv }), {});
-    return convert as User;
-  }, [data]);
-
-  const bindTelegramModal = useModal(ModalBindTelegram);
-  const telegramInvite = useQuery<{ type: string }>({
-    queryKey: [`/telegram/invite/${address}`],
-    enabled: false,
-  });
-  const telegramInviteMutate = useMutation({
-    mutationFn: async (body: { signature: string; username: string }) => {
-      const { data } = await axios.post(
-        `/api/telegram/invite/${address}`,
-        body
-      );
-      return data;
-    },
-  });
-
-  const createSignature = async () => {
-    try {
-      const linkToTelegram = document.createElement("a");
-      linkToTelegram.href = process.env.NEXT_PUBLIC_TELEGRAM_INVITE_LINK || "";
-      linkToTelegram.target = "_blank";
-      const { data } = await telegramInvite.refetch();
-      if (!data) return;
-      if (!accountMap.data?.isRegistered) {
-        linkToTelegram.click();
-        return;
-      }
-
-      if (data.type === "redirect") {
-        linkToTelegram.click();
-        return;
-      }
-
-      if (data.type === "request_bind") {
-        const username = (await bindTelegramModal.show()) as string;
-        const wallet = await getWallet();
-        const signer = wallet.getSigner();
-        const signature = await signer.signMessage(
-          getTelegramBindingSignatureMessage(username)
-        );
-        await telegramInviteMutate.mutateAsync({ signature, username });
-        window.open(process.env.NEXT_PUBLIC_TELEGRAM_INVITE_LINK, "_blank");
-      }
-    } catch (error) {
-      window.open(process.env.NEXT_PUBLIC_TELEGRAM_INVITE_LINK, "_blank");
-    }
-  };
-
   return (
     <Stack maxW="container.xl" mx={{ base: "4", lg: "auto" }}>
       <Heading
@@ -116,7 +40,6 @@ export const SectionProfileV2 = () => {
             h="100%"
             w="100%"
             rounded="2.5rem"
-            // direction={{ base: "column", md: "column", lg: "row", xl: "row" }}
             direction="column"
             justify="space-between"
             background="#091E2A"
