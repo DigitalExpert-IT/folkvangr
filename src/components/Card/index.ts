6a78e043178e3dfe20d2b2e59d6370c6fc4b4851
Y0SH4 <yosa.agung13@gmail.com>
2023-10-06 13:04:48 +0700

    update

--- src/components/Card/index.ts
+++ src/components/Card/index.ts
@@ -3,4 +3,3 @@ export * from "./CardFarmNFTv2";
 export * from "./CardProfileV2";
 export * from "./CardHomeFeaturesV2";
 export * from "./CardOwnedFarmNFTV2";
-export * from "./CardProfileBalance";
